cb853e8c1b661faf0fd0e80a7e782c08c0d21fdd
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-11 23:50:47 -0400

    Started on Matlab script to read p3d movie format

---
+++ readMovie.m
@@ -0,0 +1,77 @@
+function varargout = readMovie(num, varlist)
+% run is a 3-digit integer indicating the filenumber to read
+% varlist is a cell array of strings indicating variables to read
+% Options to add:
+% File to find movie in
+% File to save matfiles to
+% Number of frames to skip
+% Option to downsample or otherwise compress?
+
+if isInteger(num)
+    num = num2str(num,'%0.3i');
+elseif ~ischar(num)
+    error('Input RUN must be a string or integer')
+end
+
+%% Locate the variables we need
+varnames = {'rho'; 'jx'; 'jy'; 'jz'; 'bx'; 'by'; 'bz'; 'ex'; 'ey'; 'ez';
+    'ne'; 'jex';'jey'; 'jez'; 'pexx'; 'peyy'; 'pezz'; 'pexy'; 'peyz';
+    'pexz'; 'ni'; 'jix'; 'jiy'; 'jiz'; 'pixx'; 'piyy';'pizz'; 'pixy';
+    'piyz'; 'pixz'};
+
+idx = zeros(size(varlist(:)));
+for i = 1:numel(varlist)
+    idx(i) = find(strcmpi(varnames,varlist{i})); % List of where each variable is located in the order
+end
+
+%% Find system size
+fid = fopen('p3d.stdout');
+nvals = cell2mat(textscan(fid, '%*[^=] %*1s %d',3)); % [nx ny nz]
+fclose(fid);
+
+nx = nvals(1); ny = nvals(2); nz = nvals(3); 
+
+
+%% Read integer data
+
+
+skip = 0; % Number of frames to skip
+% TODO: Add an option to skip. 
+tic
+for i = 1:numel(varlist)
+    fid = fopen(['movie.' varlist{i} '.' num]);
+    data{i} = reshape(fread(fid,Inf,[num2str(nx*ny*nz) '*uint16'],2*nx*ny*nz*skip),nx,ny,nz,[]); 
+    % Could make this a matfile, do everything from here on out in memory:
+    % data{i} = matfile([varlist{i} '.' num '.mat'])
+    % data{i}.vals = reshape ...
+    % But that appears to be much slower, so let's avoid if possible.
+    fclose(fid);
+    toc
+end
+
+
+%% Normalize
+% TODO: Add an option to skip normalization?
+% Won't need it e.g. for LE calculation
+tic
+ranges = reshape(dlmread(['movie.log.' num]),1,1,1,[],2);
+for i = 1:numel(varlist)
+    nt = size(data{i},4);
+    r = ranges(:,:,:,idx(i) + (0:nt-1)*length(varnames),:); % min-max data for the current variable
+    A = 2^16./diff(r,1,5); % 1st coefficient for normalization
+    B = -A.*r(:,:,:,:,1); % 2nd coefficient for normalization
+    data{i} = A.*data{i} + B;
+    toc
+end
+
+%% Save the files
+% Only if no outputs were requested?
+% Or maybe better to do this without.
+if nargout == 0
+    for i = 1:numel(varlist)
+        assignin('caller',varlist{i},data{i});
+        save([varlist{i} '.' num '.mat'],varlist{i})
+    end
+else
+    varargout = data;
+end
