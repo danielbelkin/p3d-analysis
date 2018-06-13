6a1d335ad743f8e815c3d4ed2f3128c83f51eea5
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-12 20:31:51 -0400

    Changed write file

--- mms3_readfile.m
+++ mms3_readfile.m
@@ -3,8 +3,11 @@
 
 
 rdir = '/project/projectdirs/reconn/lorajm/mms3d';
-wdir = '/global/u2/d/dbelkin/matlab/mms3d-matfiles';
+% wdir = '/global/u2/d/dbelkin/matlab/mms3d-matfiles';
+wdir = '$SCRATCH/mms3d-matfiles';
 
+t0 = tic;
 for num = 0:4
     readMovie(num,{'bx' 'by' 'bz'},'rdir',rdir,'wdir',wdir);
 end
+toc(t0)
