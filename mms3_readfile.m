eb792c9cbbb504e24a1d4226056ee083bfd76966
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-12 20:38:32 -0400

    same thing

--- mms3_readfile.m
+++ mms3_readfile.m
@@ -4,7 +4,7 @@
 
 rdir = '/project/projectdirs/reconn/lorajm/mms3d';
 % wdir = '/global/u2/d/dbelkin/matlab/mms3d-matfiles';
-wdir = '$SCRATCH/mms3d-matfiles';
+wdir = '/scratch2/scratchdirs/dbelkin/mms3d-matfiles';
 
 t0 = tic;
 for num = 0:4
