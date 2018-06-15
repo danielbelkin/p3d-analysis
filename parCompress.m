346b3ba51ce67b520c014d9d1385fbaced2fb7d7
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-15 18:05:44 -0400

    parCompress appears to finally work

--- parCompress.m
+++ parCompress.m
@@ -12,6 +12,7 @@ function y = parCompress(x,n,p,T)
 %
 % TODO: Conside making this parallelizeable.
 % TODO: Add more options for the kernel, etc
+% PROBELM: Not currently working in parallel form. 
 
 if ~isa(x,'matlab.io.MatFile')
     error('This function is inefficient with non-matfile inputs. Try ccompress')
@@ -30,7 +31,7 @@ if nargin < 3
 end
 
 if nargin < 4
-    T = s(4); % Number of timesteps to do
+    T = 1:s(4); % Timesteps to do
 end
 
 
@@ -39,22 +40,25 @@ end
 kernel = @(x) normpdf(x,0,n/2);
 w = n; % Width = 2*w + 1, actually.
 h = kernel(-w:w);
-h = h.*reshape(h,1,[]).*reshape(h,1,1,[]); % Make it 3d;
+h = h.*reshape(h,[],1).*reshape(h,1,1,[]); % Make it 3d;
 h = single(h); % Keep the precision low
+h = h./sum(h(:)); % Renormalize
+
 
 sections = splitIndx(s(1:3),(size(h,1) - 1)/2, p); % Split the indices
 frames = cell(1,1,1,T);
-for t=1:T % For each frame
+for t = T % For each frame requested
     sectResult = cell(size(sections));  % Holds the result from each section
     disp('Splitting data...')
-    parfor i = 1:numel(sections)
-        v = convn(x.val(sections{i}{:},t),h,'valid');
+    for i = 1:numel(sections) % TODO: Make parfor
+        data = mfileIndx(x,sections{i},t);
+        v = convn(data,h,'valid');
         sectResult{i} = single(v(1:n:end,1:n:end,1:n:end)); % And downsample
     end
     disp('Recombining data...')
-    frames{t} = cell2mat(sectResult); % Recombine results
+    frames{t} = cell2mat(sectResult); % Combine sections to form a frame
 end
-y = cell2mat(frames); % And combine all frames to return.
+y = cell2mat(frames); % And combine all frames to form a movie.
 end
 
 
--- splitIndx.m
+++ splitIndx.m
@@ -1,12 +1,15 @@
 function sections = splitIndx(s,m,n)
 % SECTIONS = parCircExpand(S,M,N)
 % is used to break an array of size S into 2^N chunks, each overlapping by
-% M elements on every edge. This is useful for parallelizing access to
-% matfiles. 
-% SECTIONS is a cell array. SECTIONS{P}{D} is the list of indices for
-% processor P along dimension D. To access the desired chunk of a variable
-% X, processor P can use X(SECTIONS{P}{:}). 
-% Consider renaming to indxSplit, parIndx or something.
+% M elements on every edge. 
+% SECTIONS is a cell array. SECTIONS{P}{D} is the cell array of indices for
+% processor P along dimension D. To access chunk P of a field named .val
+% on a matfile M, use mfileIndx(M,SECTIONS{P})
+% 
+% Note: If we ever want to break non-matfiles into chuncks for some reason,
+% a more elegant approach is to change the line defineing indx to
+% indx = [s(d)-m+1:s(d), 1:s(d), 1:m]; 
+% Then processor P can access its chunk of X with X(SECTIONS{P}{:}). 
 
 if n ~= 0 && any(mod(log2(s),1))
     error('Unlikely to work')
@@ -26,8 +29,7 @@ dim = numel(s);
 insideOut = cell(1,dim); % Pre-allocate
 c = repmat({':'},1,length(s)); % Used for a cell indexing trick
 for d = 1:dim % For each dimension
-    indx = [s(d)-m+1:s(d), 1:s(d), 1:m]; % All indices along this dimension
-    
+    indx = [s(d)-m+1:s(d), 1:s(d), 1:m]; 
     insideOut{d} = cell(split); % Pre-allocate again
     for i = 1:split(d) 
         block = indx(1+s(d)/split(d)*(i-1):s(d)/split(d)*(i) + 2*m); % Subsection of indices for this dimension
@@ -38,7 +40,7 @@ for d = 1:dim % For each dimension
 end
 
 % ...and flip inside out back to right-side-out.
-sections = cell(1,2^n);
+sections = cell(split); % No, we want this to be meaningful.
 for l = 1:numel(sections)
     sections{l} = cell(1,d);
     for d = 1:dim
