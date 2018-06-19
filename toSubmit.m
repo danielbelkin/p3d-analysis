a47ca45c8c4ee6887f8c3d26514bc32292859566
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-18 23:16:11 -0400

    and another

--- toSubmit.m
+++ toSubmit.m
@@ -8,13 +8,15 @@
 addpath /global/u2/d/dbelkin/matlab/p3d-analysis
 cd /scratch2/scratchdirs/dbelkin/mms3d-compr
 
-pp = parpool('local',16);
+if isempty(gcp('nocreate'))
+    pp = parpool('local',16);
+end
 
 names = {'bx by bz'};
 val = zeros(512,256,128,3,10);
 for i=1:3
    i
    m = matfile([names{i} '.004.mat']);
-   val(:,:,:,i,:) = parCompress(m,2,16,1:10);
+   val(:,:,:,i,:) = parCompress(m,2);
 end
 save('/global/u2/d/dbelkin/matlab/bfield.004.compr.mat','val','-v7.3');
