bfd2cdc1384b16e0839a31412e341aa022684a95
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-25 11:33:40 -0400

    Setting up to read some heat3d data

---
+++ readHeat3D.m
@@ -0,0 +1,14 @@
+function readHeat3D(name)
+
+rdir = '/project/projectdirs/reconn/jdahlin/heat_3d/';
+wdir = '/scratch2/scratchdirs/dbelkin/heat3d/';
+
+nx = 512;
+ny = 256;
+nz = 256;
+
+val = dlmread(['test' name '.txt']);
+val = val';
+val = reshape(val,nx,ny,nz);
+save([wdir name '.mat'],'val','-v7.3');
+end
