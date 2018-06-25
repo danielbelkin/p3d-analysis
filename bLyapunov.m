a6e072d656a1dbf144d88e97827caf6a94227089
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-25 19:28:26 -0400

    Added ergodic measure awareness to bLyapunov

--- bLyapunov.m
+++ bLyapunov.m
@@ -1,22 +1,38 @@
-function [lambda, stats] = bLyapunov(Bfield)
+function [lambda, stats] = bLyapunov(Bfield,isIn)
 % [LAMBDA, STATS] = bLyapunov(BFIELD)
 % Function to compute lyapunov exponents all in one go.
 % Works quite quickly on compressed data; much too slow for raw data.
 % Based on the assumption that the ergodic measure is unique. 
 % BFIELD = cat(4,BX,BY,BZ) 
 % This could be parallelized, but it's probably not worth it.
+%
+% [LAMBDA, STATS] = bLyapunov(BFIELD,isIn) allows you to add a logical isIn
+% which specifies a region. Use ergodicMeasure to generate it. 
 % 
 % Stats contain: 
 %   avgJ, the weighted average Jacobian
-%   stdJ, the standard deviation of avgJ based on the (incorrect)
+%   stdJ, the standard deviation of avgJ bmased on the (incorrect)
 %         assumption that difference between every 18th pair of points is
 %         iid
 %   netB, the sum of abs(B) over the region
 %   Lambda, the matrix from which lyapuonv exponents are calculated
+
 disp('Running bLyapunov...')
+
+if nargin < 2
+    isIn = 1; % By default, include whole space
+elseif any(isIn(:) > 1)
+    isIn = isIn > 0; % Only use the logical. 
+end
+
 B = sqrt(sum(Bfield.^2,4));
-netB = sum(B(:));
-bfield = Bfield./B;
+bfield = Bfield./B; % Unit vector field
+mu = B(:).*isIn(:); % Compute unnormalized measure
+netB = sum(mu);
+mu = mu/netB; % Normalize
+
+
+
 
 % Construct the gradient operator
 d = cat(3,-ones(3),zeros(3),ones(3)); % This gives 18 times the gradient.
@@ -27,8 +43,8 @@ stdJ = avgJ;
 for i = 1:3
     for j = 1:3
         Jij = cconvn(bfield(:,:,:,i),grad(:,:,:,j))/18;
-        avgJ(i,j) = sum(Jij(:).*B(:))./netB; % The weighted average
-        stdJ(i,j) = sqrt(sum(Jij(:).^2.*B(:))./netB - avgJ(i,j)^2);
+        avgJ(i,j) = sum(Jij(:).*mu); % The weighted average
+        stdJ(i,j) = sqrt(sum(Jij(:).^2.*mu) - avgJ(i,j)^2);
     end
 end
 
