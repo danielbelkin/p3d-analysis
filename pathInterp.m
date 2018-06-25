5fbcc6dd26bdafadd3a726253a4278d63b7e800d
danielbelkin <dbelkin1@swarthmore.edu>
2018-06-25 19:35:40 -0400

    bug hunt

--- pathInterp.m
+++ pathInterp.m
@@ -10,6 +10,11 @@ if nargin < 2
 end
 
 svals = [0; cumsum(sum(diff(path).^2,2))]; % Arc-length at each point
+
+if any(diff(svals) == 0)
+    path
+    error('Singularity in speed encountered?')
+end
 pathfun = @(s) [interp1(svals,path(:,1),s),...
     interp1(svals,path(:,2),s),...
     interp1(svals,path(:,3),s)];
