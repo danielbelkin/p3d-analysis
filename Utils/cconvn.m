6af3d9e5d612ed4c3c7b9d583cd160cd1c48fd56
danielbelkin <dbelkin1@swarthmore.edu>
2018-07-18 20:52:13 -0400

    Now operates only along first 3 dimensions
    M

--- Utils/cconvn.m
+++ Utils/cconvn.m
@@ -1,6 +1,11 @@
 function x = cconvn(x,h)
 % X = cconv(X,H) circularly convolves an array X with a kernel h.
+% TODO: Make it operate along only the first 3 dimensions.
+s = size(x);
 n = size(h,1);
-indx = circExpand(size(x),(n-1)/2);
-x = convn(x(indx{:}),h,'valid'); % This is bigger by 2 along each axis.
+indx = circExpand(s(1:3),(n-1)/2);
+T = prod(s(4:end));
+for i=1:T
+    x(:,:,:,T) = convn(x(indx{:},T),h,'valid'); % This is bigger by 2 along each axis.
+end
 end
