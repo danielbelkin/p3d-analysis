0d00e5f2e9a11e9d3f29c1c8a6f919504de4a8d3
danielbelkin <dbelkin1@swarthmore.edu>
2018-07-18 21:03:12 -0400

    Extended functionality to non-cubic kernels

--- Utils/cconvn.m
+++ Utils/cconvn.m
@@ -2,10 +2,12 @@ function x = cconvn(x,h)
 % X = cconv(X,H) circularly convolves an array X with a kernel h.
 % TODO: Make it operate along only the first 3 dimensions.
 s = size(x);
-n = size(h,1);
-indx = circExpand(s(1:3),(n-1)/2);
+n = size(h); % Want to make sure it's length 3
+n(end+1:3) = 1;
+
+indx = circExpand(s(1:3),(n-1)/2); % Want to be able to exand for a 
 T = prod(s(4:end));
 for i=1:T
-    x(:,:,:,T) = convn(x(indx{:},T),h,'valid'); % This is bigger by 2 along each axis.
+    x(:,:,:,i) = convn(x(indx{:},i),h,'valid'); % This is bigger by 2 along each axis.
 end
 end
--- Utils/circExpand.m
+++ Utils/circExpand.m
@@ -1,9 +1,12 @@
 function indx = circExpand(s,m)
 % Takes a size s and an integer m.
-% Returns a cell array such that x
+% Returns a cell array such that x(indx{:}) is expanded.
+% if m is a vector of same length as s, then each element is used.
+
+m = m + zeros(size(s)); % Expand to same size as s
 dim = numel(s);
 indx = cell(1,dim);
 for d = 1:dim
-    indx{d} = mod([s(d)-m+1:s(d), 1:s(d), 1:m] - 1, s(d)) + 1;
+    indx{d} = mod([s(d)-m(d)+1:s(d), 1:s(d), 1:m(d)] - 1, s(d)) + 1;
 end
 end
