79b3ff42ebd60a28a464bc46e328c60040ba23ec
danielbelkin <dbelkin1@swarthmore.edu>
2018-07-27 11:53:47 -0400

    It works now?
    Except for overall scale, possibly

--- vectorPotential2.m
+++ vectorPotential2.m
@@ -1,17 +1,23 @@
 function A = vectorPotential2(J)
-% Gonna use fourier transform methods
-% Take fft, multiply by 1./k^2, 
-kx = reshape(0:size(J,1)-1,[],1,1);
-ky = reshape(0:size(J,2)-1,1,[],1);
-kz = reshape(0:size(J,3)-1,1,1,[]);
+% A = vectorPotential2(J)
+% Uses fourier transform methods
+% WARNING: Scale may be wrong.
+%
+% But really, we want to be able to do this with vector currents. Need to
+% check that it still works with the extra dimensions.
+% (And it would be slightly faster to construct an fft3 instead of using fftn, I
+% suspect)
+%
+
+freqs = @(s) [0:floor(s/2) ceil(s/2)-1:-1:1]; % I think this is right
+
+s = size(J);
+[kx,ky,kz] = meshgridn(freqs(s(1)),freqs(s(2)),freqs(s(3)));
 
 k2 = kx.^2 + ky.^2 + kz.^2;
-k2(1) = 1; % Avoid the infinity (this is not the right way to do this)
-
 
 H = 1./k2;
-A = -real(ifftn(fftn(J).*H));
-
-% J(k) = -k^2 A(k), I think
+H(1) = 0; % Subtract off mean current, in effect
+A = ifftn(fftn(J).*H);
 end
 
